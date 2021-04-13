8f932daff79b2a76890d88f10f94bf86967870f8
zzkevin <realzzkevin@gmail.com>
2021-04-13 19:53:00 -0400

    add basic search function

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 var cdbKey ='1';
 var cname;
-var cByName =`search.php?s=${cname}`;
-var cDBcriteria = cByName;
+//var cByName =`search.php?s=${cname}`;
+//var cDBcriteria = cByName;
 var food;
 //ingredients will be stored in an array.
-var fIngredients;
+var fIngredients=[];
 
 var spoonKey = "4d74df0d2f4a433e9df53519bb28d05a";
 var recipes='complexSearch';
 var fCriteria = recipes;
 var cocktail;
-var CTIngredients;
+var CTIngredients=[];
 
 async function searchRecipes(name) {
     
@@ -19,16 +19,29 @@ async function searchRecipes(name) {
     console.log(spoonUrl);
 
     fetch(spoonUrl)
+    
         .then(function (response){
 
             if(response.ok){
                 
-                food = response.json();
-            
+                //food = response.json();            
 
-                food.then(function(data){
+                response.json().then(function(data){
 
-                    console.log(food);
+                    //food = data;
+
+                    console.log(data);
+
+                    var foodList = data.results;
+
+                    //console.log(food.results);
+
+                    console.log(foodList[0].id);
+
+                    return foodList;
+
+                    getIngredients(foodList[0].id);
+
                 });
 
 
@@ -41,15 +54,72 @@ async function searchRecipes(name) {
     
     
 }
-
-async function getIngredients() {
+// get ingredients by id
+async function getIngredients(id) {
     
+    var url = `https://api.spoonacular.com/recipes/${id}/ingredientWidget.json?apiKey=${spoonKey}`;
+    //
+    console.log(url);
+
+    fetch(url)
+
+        .then(function(response){
+            if(response.ok){
+
+                //ingre = response.json();
+
+                response.json().then(function(data){
+
+                   // ingre = data;
+
+                    console.log(data);
+
+                    var ingreList = data.ingredients;
+
+                    for(var i=0; i<ingreList.length; i++){
+
+                        fIngredients.push(ingreList[i].name);
+
+                    }
+
+                    console.log(fIngredients);
+
+                })
+
+            }else{
+                //return error
+            }
+        })
     
 }
+//search cocktails by name
+async function searchCocktail(name) {
 
-async function searchCoktail(name) {
-    
-    var ThecocktailDBUrl = `https://www.thecocktaildb.com/api/json/v1/1/${cDBcriteria}`;
+    var ThecocktailDBUrl = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${name}`;
+
+    fetch(ThecocktailDBUrl)
+        .then(function(response){
+
+            if(response.ok){
+                //cocktail = response.json();
+
+                response.json().then(function(data){
+
+                    //cocktail = data;
+
+                    console.log(data);
+
+                    var list = data.drinks[0];
+
+                    //while ( )
+
+
+                })
+            } else {
+
+                //return error
+            }
+        });
 
 }
 
@@ -58,4 +128,10 @@ async function getCTingredient(cTname) {
 
 }
 
-searchRecipes('pasta');
+function displayReceipeOpt(){
+
+    
+}
+
+searchRecipes('spagetti');
+searchCoktail('Screwdriver');
