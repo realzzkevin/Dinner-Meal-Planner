c7939e47fab3bee3aa0af7e00d35e48e8e023310
zzkevin <realzzkevin@gmail.com>
2021-04-14 18:11:59 -0400

    add event listeners

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 var cdbKey ='1';
 var cname;
 
-//var cByName =`search.php?s=${cname}`;
-//var cDBcriteria = cByName;
-
 var food;
 //ingredients will be stored in an array.
 var fIngredients=[];
@@ -14,9 +11,10 @@ var fCriteria = recipes;
 var cocktail;
 var CTIngredients=[];
 
+//search recipes by their name. return a list of recipes;
 async function searchRecipes(name) {
     
-    var spoonUrl = `https://api.spoonacular.com/recipes/${fCriteria}?apiKey=${spoonKey}&query=${name}`;
+    var spoonUrl = `https://api.spoonacular.com/recipes/${fCriteria}?apiKey=${spoonKey}&query=${name}&number=5`;
 
     console.log(spoonUrl);
 
@@ -25,8 +23,6 @@ async function searchRecipes(name) {
         .then(function (response){
 
             if(response.ok){
-                
-                //food = response.json();            
 
                 response.json().then(function(data){
 
@@ -34,26 +30,30 @@ async function searchRecipes(name) {
 
                     console.log(data);
 
+                    // a list of recipeis
                     var foodList = data.results;
 
                     //console.log(food.results);
                     console.log(foodList);
 
-                    console.log(foodList[0].id);
+                   // console.log(foodList[0].id);
 
-                    return foodList;
+                    showRecipetList(foodList);
 
+                    
                     getIngredients(foodList[0].id);
-
+                    
+                    return foodList;
                 });
 
 
             }else {
 
+                throw new Error(`HTTP Error! status: ${response.status}`);
                 //return error
             }
 
-        })
+        });
     
     
 }
@@ -62,14 +62,12 @@ async function getIngredients(id) {
     
     var url = `https://api.spoonacular.com/recipes/${id}/ingredientWidget.json?apiKey=${spoonKey}`;
     //
-    console.log(url);
+    //console.log(url);
 
     fetch(url)
 
         .then(function(response){
             if(response.ok){
-
-                //ingre = response.json();
 
                 response.json().then(function(data){
 
@@ -81,7 +79,7 @@ async function getIngredients(id) {
 
                     for(var i=0; i<ingreList.length; i++){
 
-                        fIngredients.push(ingreList[i].name);
+                        fIngredients.push(ingreList[i].name +': '+ingreList[i].amount.us.value+' '+ingreList[i].amount.us.unit);
 
                     }
 
@@ -91,6 +89,7 @@ async function getIngredients(id) {
 
             }else{
                 //return error
+                throw new Error(`HTTP Error! status: ${response.status}`);
             }
         })
     
@@ -112,29 +111,119 @@ async function searchCocktail(name) {
 
                     console.log(data);
 
-                    var list = data.drinks[0];
+                    var list = data.drinks;
 
-                    //while ( )
+                    getCTingredient(list[0]);
 
 
                 })
             } else {
 
                 //return error
+
+                throw new Error(`HTTP Error! status: ${response.status}`);
             }
         });
 
 }
 
-async function getCTingredient(cTname) {
+// need find a way to make it work later.
+function getCTingredient(cTname) {
+    
+    var i =1;
 
+    var ingre =`strIngredient${i}`;
+
+    while(cTname.ingre!=null){
+
+
+    }
 
 }
 
-function displayReceipeOpt(){
+// take a list of recipets display them in a group of tab.
+function showRecipetList(list){
+
+    
+    var tabEl= $('<div>').attr('id','tabs');
+    var ulEl = $('<ul>');
+    tabEl.append(ulEl);
+    
+    for (var i=0; i<list.length; i++){
+        var liEl = $('<li>');
+        var aEl = $('<a>').attr('href', "#tab-"+i);
+        aEl.text(list[i].title);
+        liEl.append(aEl);
+        ulEl.append(liEl);
+
+        var subTabEl = $('<div>').attr('id',"tab-"+i);
+        var pEl = $('<p>').text(list[i].title);
+        var imgEl = $('<img>').attr('src', list[i].image);
+        var btnEl =$('button').attr('id', list[i].id);
+
+        btnEl.append(imgEl);
+        subTabEl.append(pEl);
+        subTabEl.append(btnEl);
+
+        tabEl.append(subTabEl);
+    }
+
+    console.log(tabEl);
+    
+    $('#tabs').tabs();
+
+    return tabEl;
+
 
     
 }
 
-searchRecipes('spaghetti');
-searchCocktail('Screwdriver');
+function showCocktailList(list){
+
+
+}
+
+// attach recipet to index.html
+function addRecipet(recipet){
+
+
+}
+
+function addCocktail(){
+
+
+}
+
+
+
+//searchRecipes('spaghetti');
+//searchCocktail('Screwdriver');
+
+$('form').submit(function( event ){
+
+    event.preventDefault();
+
+    //console.log($(this).children('input').val());
+
+    var arg = $(this).children('input').val();
+    
+    str = arg.replaceAll(' ','+');
+
+    console.log(str);
+
+    var list;
+    
+    async function go() {
+        list = await searchRecipes(str);
+
+        console.log(list);
+    }
+
+    go();
+
+    console.log(list);
+
+});
+
+
+$()
